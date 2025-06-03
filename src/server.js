0b58abc582331c34c73cc3c50ebcf3a961dc5aed
phuocnguye10051307 <phuocmoi4321@gmail.com>
2025-06-03 08:19:26 +0700

    template (view ) engine

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-const express = require('express')// common js 
+const express = require('express')// common js
+const path = require('path') 
 const app = express()// app express 
 const port = 8000; // port 
 
+// config template engine
+app.set('views',path.join(__dirname,'views'))
+// paht.join là thư viên tìm kiếm 1 file trên hệ thống đã có sẵn trong js 
+app.set('view engine', 'ejs')
 
-
-// khai báo routes 
+// khai báo route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('example')
 })
 app.get('/abc', (req, res) => {
   res.send('my name is phuoc ')
@@ -15,6 +19,7 @@ app.get('/abc', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<h1>hello world</h1>')
 })
+
 
 
 app.listen(port, () => {
