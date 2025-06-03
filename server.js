398bc93fd6d882f1d83cb048baff07f46a47a0da
phuocnguye10051307 <phuocmoi4321@gmail.com>
2025-06-03 07:54:50 +0700

    more routes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express')// common js 
+const app = express()// app express 
+const port = 8000; // port 
 
+
+
+// khai báo routes 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/abc', (req, res) => {
+  res.send('my name is phuoc ')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>hello world</h1>')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
